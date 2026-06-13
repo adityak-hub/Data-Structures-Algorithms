ba8444cbcd42013c99d48f799a184c765af2f610
aditya <kaditya41057@gmail.com>
2026-06-13 21:24:56 +0530

    square pattern

---
+++ loops/forLoops_squarePattern.java
@@ -0,0 +1,11 @@
+package loops;
+
+public class forLoops_squarePattern {
+    public static void main(String[]args){
+        for(int i=1; i<=4; i++){
+             System.out.println("****");
+  
+        }
+    }
+    
+}
