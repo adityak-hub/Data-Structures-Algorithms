92346bd6edd0a9352da1f1025a5d62ec9ec1c371
aditya <kaditya41057@gmail.com>
2026-06-13 20:20:13 +0530

    ternary operator

---
+++ conditional_Questions/ternary_operator.java
@@ -0,0 +1,11 @@
+package conditional_Questions;
+
+public class ternary_operator {
+    public static void main(String[]args){
+        int number =11;
+        String result = (number%2==0)?"Even":"Odd";
+        System.out.println(result);
+    }
+    
+    
+}
