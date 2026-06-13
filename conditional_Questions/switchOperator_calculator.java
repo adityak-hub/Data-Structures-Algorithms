b8e966837494ef4be254f13fb536221915e5aee8
aditya <kaditya41057@gmail.com>
2026-06-13 20:37:55 +0530

    calculator done by switch operator

---
+++ conditional_Questions/switchOperator_calculator.java
@@ -0,0 +1,32 @@
+package conditional_Questions;
+import java.util.*;
+
+
+public class switchOperator_calculator {
+    public static void main(String[]args){
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter first number:");
+    int a = sc.nextInt();
+
+    System.out.print("Enter second number: ");
+    int b = sc.nextInt();
+
+    System.out.print("Enter operator: ");
+    char operator = sc.next().charAt(0);
+
+    switch(operator){
+        case '+' : System.out.print(a+b);
+        break;
+        case '-' : System.out.print(a-b);
+        break;
+        case '*' : System.out.print(a*b);
+        break;
+        case '/' : System.out.print(a/b);
+        break;
+        case '%' : System.out.print(a%b);
+        break;
+        default : System.out.print("wrong operator");
+    }
+    
+    }
+}
