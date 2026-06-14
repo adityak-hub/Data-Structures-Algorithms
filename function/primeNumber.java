1293341f5a619838be4dbdda33572f5a03f79e33
aditya <kaditya41057@gmail.com>
2026-06-14 14:43:07 +0530

    prime Number

---
+++ function/primeNumber.java
@@ -0,0 +1,21 @@
+package function;
+
+public class primeNumber {
+    public static boolean isPrime(int n){
+        if(n<=1){
+            return false;
+        }
+        
+        for(int  i=2; i<=n-1; i++){
+            if(n%i==0){     //completely divide
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[]args){
+        System.out.println(isPrime(1));
+
+    }
+    
+}
