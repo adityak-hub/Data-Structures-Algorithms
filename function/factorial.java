6e72c8d1a8a9205cd008534cf230fd97671aa924
aditya <kaditya41057@gmail.com>
2026-06-14 12:16:23 +0530

    factorial

---
+++ function/factorial.java
@@ -0,0 +1,24 @@
+package function;
+import java.util.*;
+
+public class factorial {
+    public static int factorial(int n){
+        
+        int fact = 1;
+
+        for(int i=1; i<=n; i++){
+             fact = fact*i;
+        }
+        return fact;
+
+
+    }
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int fact = factorial(n);
+        System.out.print(fact);
+
+    }
+    
+}
