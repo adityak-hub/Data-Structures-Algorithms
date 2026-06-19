22844897b2bcf08f98fe0931ec652367e2705e0e
aditya <kaditya41057@gmail.com>
2026-06-19 14:18:28 +0530

    max sum of an array by  kaden's algorithm

---
+++ arrays/maxSubArraySumByKadanes.java
@@ -0,0 +1,23 @@
+package arrays;
+
+public class maxSubArraySumByKadanes {
+    public static void maxSubArray(int arr[]) {
+        int ms = Integer.MIN_VALUE;
+        int cs = 0;
+
+        for(int i=0; i<arr.length; i++){
+            cs += arr[i];
+            if(cs<0){
+                cs=0;
+            }
+            ms = Math.max(ms,cs);
+        }
+        System.out.println(ms);
+        
+    }
+    public static void main(String[] args) {
+        int arr[] = {-1,-2,-3,-4};
+        maxSubArray(arr);
+    }
+    
+}
