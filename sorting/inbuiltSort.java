6672e72111dd12594b2f1c1aff7af6b7bd05f7f5
aditya <kaditya41057@gmail.com>
2026-06-20 20:58:01 +0530

    add one more method Collections to make sorting in descending order

--- sorting/inbuiltSort.java
+++ sorting/inbuiltSort.java
@@ -12,13 +12,13 @@ import java.util.Arrays;  // we can use *(full package access) instead of Arrays
     }
     
 }
-    */
+*/
 
 
 
 //sorting by index(arr,si,ei);
 
-public class inbuiltSort {
+/* public class inbuiltSort {
     public static void main(String[] args) {
         int arr[] ={5,4,0,1,3,2};
         Arrays.sort(arr,1,4);
@@ -29,3 +29,40 @@ public class inbuiltSort {
     }
     
 }
+*/
+
+
+
+// dorting in descending order by collection method
+
+/* import java.util.Collections;
+
+public class inbuiltSort {
+    public static void main(String[] args) {
+        Integer arr[] ={5,4,0,1,3,2};   //ab ye arr Integer class ka object hai primitive(basic) int pe ye collection kaam nhi karta
+        Arrays.sort(arr,Collections.reverseOrder());
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+
+    }
+    
+}
+*/
+
+
+
+
+import java.util.Collections;
+public class inbuiltSort {
+    public static void main(String[] args) {
+        Integer arr[] ={5,4,0,1,3,2};   
+        Arrays.sort(arr,2,arr.length,Collections.reverseOrder());
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+
+    }
+    
+}
+
