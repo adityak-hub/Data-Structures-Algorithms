b8a54334b94ab6c164fc07ee34c0082f4781075b
aditya <kaditya41057@gmail.com>
2026-06-25 13:49:34 +0530

    sum of diagonals of matrix

---
+++ array2D/diagonalSum.java
@@ -0,0 +1,37 @@
+package array2D;
+public class diagonalSum {
+    public static int diagnolSum(int matrix[][]) {
+        int sum=0;
+
+        // for(int i=0; i<matrix.length; i++){          //brute force code
+        //     for(int j=0; j<matrix[0].length; j++){
+        //         //pd
+        //         if(i==j){
+        //             sum += matrix[i][j];
+        //         }
+        //         else if(i+j==matrix.length-1){
+        //             sum+=matrix[i][j];
+        //         }
+        //     }
+        // }
+        // return sum;
+
+        for(int i=0; i<matrix.length; i++){   // optimized code
+            //pd
+            sum += matrix[i][i];
+
+            //sd
+            if( i != matrix.length-1-i)
+              sum += matrix[i][matrix.length-i-1];
+
+        }
+        return sum;
+
+         
+        
+    }
+    public static void main(String[] args) {
+        int matrix[][] = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+        System.out.print(diagnolSum(matrix));
+    }
+}
