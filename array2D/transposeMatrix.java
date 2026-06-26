97d557c19923253f2bb22330bb861fc5b0547f46
aditya <kaditya41057@gmail.com>
2026-06-26 16:56:17 +0530

    little bit change

--- array2D/transposeMatrix.java
+++ array2D/transposeMatrix.java
@@ -2,7 +2,7 @@ package array2D;
 
 public class transposeMatrix {
     public static void transposeMatrix(int matrix[][]) {
-        int transpose[][] = new int[matrix[0].length][matrix.length];
+        int transpose[][] = new int[matrix.length][matrix[0].length];
         for(int i=0; i<=matrix.length-1; i++){
             for(int j=0; j<=matrix[0].length-1; j++){
                 transpose[j][i]=matrix[i][j];
@@ -18,7 +18,7 @@ public class transposeMatrix {
         }
     }
     public static void main(String[] args) {
-        int matrix[][] = {{1,2,3},{4,5,6},{7,8,9}};
+        int matrix[][] = {{3,7,9},{0,1,8},{2,6,4}};
         transposeMatrix(matrix);
         
     
