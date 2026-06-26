335965fbbce9f222879da569eed391ec65249a9e
aditya <kaditya41057@gmail.com>
2026-06-26 17:58:11 +0530

    String basic methods

---
+++ strings/stringMethods.java
@@ -0,0 +1,42 @@
+package strings;
+import java.util.*;
+
+public class stringMethods {
+    public static void main(String[] args) {
+        /* 
+        String str = "abcd1";
+        String str1 = new String();
+        str1="aditya";
+        System.out.println(str);
+        System.out.println(str1);
+        */
+
+        //Strings are immutable
+
+        Scanner sc = new Scanner(System.in);
+        /* 
+        String name;
+        name=sc.nextLine();
+        System.out.print(name);
+        */
+
+        /* 
+        String fullName = "Aditya Kumar";
+        System.out.println(fullName.length());
+        */
+
+
+        /*String firstName = "Aditya";
+        String lastName = "Kumar";
+        String fullName = firstName + " " + lastName;
+        System.out.println(fullName);
+        */
+
+        String fullName = "Aditya kumar";
+        System.out.println(fullName.charAt(1));
+
+
+
+        
+    }
+}
