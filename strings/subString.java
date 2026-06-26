0426b7e28d5977348a9cfad3c3850d6a097f24b8
aditya <kaditya41057@gmail.com>
2026-06-26 22:56:19 +0530

    find out substring by creating function or by inbuilt function

---
+++ strings/subString.java
@@ -0,0 +1,18 @@
+package strings;
+
+public class subString {
+    public static String subString(String str,int si,int ei) {
+        String substr= "";
+        for(int i=si; i<=ei-1; i++){
+            substr += str.charAt(i);
+        }
+        return substr;
+        
+    }
+    public static void main(String[] args) {
+        String str = "helloworld";
+        // System.out.println(str.substring(0,5));   function for substring
+        System.out.println(subString(str, 0, 5));
+    }
+    
+}
