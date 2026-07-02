33163d15e977e948177dc4933feade9e092d9d1f
aditya <kaditya41057@gmail.com>
2026-07-02 22:57:36 +0530

    make ith bit 1

---
+++ bit_Manipulation/setIthBit.java
@@ -0,0 +1,15 @@
+package bit_Manipulation;
+
+public class setIthBit {
+    public static int setIthBit(int n,int i) {
+
+        int bitmask = 1<<i;
+        return n | bitmask ;
+        
+    }
+    public static void main(String[] args) {
+        System.out.println(setIthBit(10, 2));
+        
+    }
+    
+}
