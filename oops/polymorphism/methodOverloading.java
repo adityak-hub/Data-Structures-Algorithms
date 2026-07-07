7c084c340d80bd44b7b8aad9079c716f838ef64f
aditya <kaditya41057@gmail.com>
2026-07-07 23:14:54 +0530

    compile time overloading(it is achieved by method overloading)

---
+++ oops/polymorphism/methodOverloading.java
@@ -0,0 +1,26 @@
+package oops.polymorphism;
+
+import array2D.searchKeyInSortedMatrix;
+
+public class methodOverloading {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println(calc.sum(3,5));
+        System.out.println(calc.sum(3.4f,5.2f));
+        System.out.println(calc.sum(3,5,9));
+    }
+}
+
+class Calculator{
+    int sum(int a,int b){
+        return a+b;
+    }
+
+    float sum(float a, float b){
+        return a+b;
+    }
+
+    int sum(int a,int b,int c){
+        return a+b+c;
+    }
+}
