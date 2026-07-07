1fab0a0697c0676e618ed49e9fbbbf6a140919f0
aditya <kaditya41057@gmail.com>
2026-07-07 17:04:33 +0530

    add in constructor folder

---
+++ oops/constructor/constructor.java
@@ -0,0 +1,25 @@
+package oops.constructor;
+
+public class constructor{
+    public static void main(String[] args) {
+        Student1 s1 = new Student1();
+        
+        Student1 s2 = new Student1("aditya");
+        System.out.println(s2.name);
+    }
+}
+
+
+class Student1{
+    String name;
+    int roll;
+
+    Student1(String name){  //this is constructor it doen't have return type and it is same as class
+        
+        this.name = name;
+    }
+    Student1(){  //this is constructor it doen't have return type and it is same as class
+        
+        System.out.println("constructor is calling..");
+    }
+}
