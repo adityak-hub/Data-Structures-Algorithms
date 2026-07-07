3e471844debbe82d1b367512ba2e2c6d7db7db60
aditya <kaditya41057@gmail.com>
2026-07-07 23:17:29 +0530

    Compile time polymorphism(achieved by method overloading

--- oops/polymorphism/methodOverloading.java
+++ oops/polymorphism/methodOverloading.java
@@ -6,7 +6,7 @@ public class methodOverloading {
     public static void main(String[] args) {
         Calculator calc = new Calculator();
         System.out.println(calc.sum(3,5));
-        System.out.println(calc.sum(3.4f,5.2f));
+        System.out.println(calc.sum(3.4f,5.2f)); //or float(3.4),float(5.2)
         System.out.println(calc.sum(3,5,9));
     }
 }
