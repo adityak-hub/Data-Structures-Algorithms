9f8d3ff8f3c6108d834ece0330681a8f0279f1f7
aditya <kaditya41057@gmail.com>
2026-07-08 22:15:05 +0530

    just change class name due to it is same in another file

--- oops/constructor/shallowCopyConstructor.java
+++ oops/constructor/shallowCopyConstructor.java
@@ -2,7 +2,7 @@ package oops.constructor;
 
 public class shallowCopyConstructor {
     public static void main(String[] args) {
-        Student s1 = new Student();
+        Student1 s1 = new Student1();
         s1.name = "aditya";
         s1.roll=456;
         s1.password = "abcd";
@@ -10,7 +10,7 @@ public class shallowCopyConstructor {
         s1.marks[1]=90;
         s1.marks[2]=80;
 
-        Student s2 = new Student(s1);   //copy s1 in s2
+        Student1 s2 = new Student1(s1);   //copy s1 in s2
         s2.password = "xyz";
         s1.marks[2]=100;
         for(int i=0; i<=2; i++){
@@ -19,20 +19,20 @@ public class shallowCopyConstructor {
     }
 }
 
-class Student{
+class Student1{
     String name;
     int roll;
     int marks[];
     String password;
   
     //It is shallow copy constructor  because it copy references
-    Student(Student s1){
+    Student1(Student1 s1){
         marks = new int[3];
         this.name = s1.name;
         this.roll = s1.roll;
         this.marks = s1.marks;
     }
-     Student(){
+     Student1(){
         marks = new int[3];
         System.out.println("constructor is called...");
     }
