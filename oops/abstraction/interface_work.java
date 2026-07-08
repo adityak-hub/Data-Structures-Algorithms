9b85f28d628d1980f880abeba445af901ff41df7
aditya <kaditya41057@gmail.com>
2026-07-08 09:18:56 +0530

    how interface works

---
+++ oops/abstraction/interface_work.java
@@ -0,0 +1,30 @@
+package oops.abstraction;
+
+public class interface_work {
+    public static void main(String[] args) {
+        Queen q = new Queen();
+        q.moves();
+    }
+}
+
+interface ChessPlayer{
+    void moves();
+}
+
+class Queen implements ChessPlayer{
+    public void moves(){
+        System.out.println("up, down, left, right, diagonal(in all 4 direction)");
+    }
+}
+
+class Rook implements ChessPlayer{
+    public void moves(){
+        System.out.println("up, down, left, right");
+    }
+}
+
+class king implements ChessPlayer{
+    public void moves(){
+        System.out.println("up, down, left, right,(by 1 step");
+    }
+}
